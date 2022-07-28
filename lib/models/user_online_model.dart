2c89d70151a98a3d7937d749d1d222bb980b27b8
tuvh <tuvh.itc@vietravel.com>
2022-07-28 16:49:01 +0700

    add model

---
+++ lib/models/user_online_model.dart
@@ -0,0 +1,33 @@
+
+
+class UserOnLineModel{
+  static const userOnLineUid ='uid';
+  static const userOnLineName ='name';
+  static const userOnLineLastAccess ='lastAccess';
+  static const userOnLineIsOnline ='isOnline';
+
+  String? uid;
+  String? name;
+  String? lastAccess;
+  bool? isOnline;
+  UserOnLineModel({this.uid,this.name,this.lastAccess,this.isOnline});
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data =  Map<String, dynamic>();
+    data[userOnLineUid] = uid;
+    data[userOnLineName] = name;
+    data[userOnLineLastAccess] = lastAccess;
+    data[userOnLineIsOnline] = isOnline;
+    return data;
+  }
+  factory UserOnLineModel.fromJson(Map<String, dynamic> json)=>UserOnLineModel(
+      uid: json[userOnLineUid],
+      name: json[userOnLineName],
+      lastAccess: json[userOnLineLastAccess],
+      isOnline: json[userOnLineIsOnline]
+  );
+
+  @override
+  String toString() {
+    return 'UserOnLineModel{uid: $uid, name: $name, lastAccess: $lastAccess, isOnline: $isOnline}';
+  }
+}
